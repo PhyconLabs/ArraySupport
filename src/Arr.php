feafb18cdb2dacc0bf393bb69ff811975cc3505c
Rolands Atvars <rolands.atvars@gmail.com>
2014-12-04 23:32:12 +0200

    Improved deep merge functionality.

--- src/Arr.php
+++ src/Arr.php
@@ -1,20 +1,27 @@
 <?php
 namespace SDS\ArraySupport;
 
-use \ArrayObject;
+use \ArrayAccess;
 
-class Arr extends ArrayObject
+class Arr implements ArrayAccess
 {
+    protected $array;
+    
     public function __construct(array $array = [])
     {
-        parent::__construct($array);
+        $this->array = $array;
     }
     
-    public function offsetGetDeep($offset, $default = null)
+    public function offsetGet($offset)
+    {
+        return isset($this->array[$offset]) ? $this->array[$offset] : null;
+    }
+    
+    public function offsetDeepGet($offset, $default = null)
     {
         $offsets = explode(".", $offset);
         $lastOffset = array_pop($offsets);
-        $currentLevel =& $this;
+        $currentLevel =& $this->array;
         
         foreach ($offsets as $offset) {
             if (isset($currentLevel[$offset]) && is_array($currentLevel[$offset])) {
@@ -27,8 +34,50 @@ class Arr extends ArrayObject
         return isset($currentLevel[$lastOffset]) ? $currentLevel[$lastOffset] : $default;
     }
     
-    public function offsetExistsDeep($offset)
+    public function offsetSet($offset, $value)
     {
-        return !is_null($this->offsetGetDeep($offset));
+        $this->array[$offset] = $value;
+    }
+    
+    public function offsetUnset($offset)
+    {
+        unset($this->array[$offset]);
+    }
+    
+    public function offsetExists($offset)
+    {
+        return isset($this->array[$offset]);
+    }
+    
+    public function offsetDeepExists($offset)
+    {
+        return !is_null($this->offsetDeepGet($offset));
+    }
+    
+    public function merge(array ...$arrays)
+    {
+        foreach ($arrays as $array) {
+            $this->array = array_merge($this->array, $array);
+        }
+        
+        return $this;
+    }
+    
+    public function deepMerge(array ...$arrays)
+    {
+        foreach ($arrays as $array) {
+            $this->array = array_replace_recursive($this->array, $array);
+        }
+        
+        return $this;
+    }
+    
+    public function deepAppendMerge(array ...$arrays)
+    {
+        foreach ($arrays as $array) {
+            $this->array = array_merge_recursive($this->array, $array);
+        }
+        
+        return $this;
     }
 }
